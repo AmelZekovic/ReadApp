6d2e96d844e177fa4e9c12ff9648a5cab2770950
amelzekovic <amelzekovic@gmail.com>
2018-02-25 19:54:38 +0100

    First commit

---
+++ ReadApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = ConfigurationManager.ConnectionStrings["Northwind"].ConnectionString;
+
+            try
+            {
+                using (conn)
+                {
+                    conn.Open();
+                    string sql = "SELECT Products.ProductID, Products.ProductName, Categories.CategoryName FROM Products LEFT JOIN Categories ON Products.CategoryID=Categories.CategoryID";
+                    SqlCommand command = new SqlCommand(sql, conn);
+                    SqlDataReader reader;
+                    using (reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(reader["ProductName"] + ", " + reader["CategoryName"]);
+                        }
+                    }
+                }
+
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("Error reading the database");
+                Console.WriteLine(err.Message);
+            }
+
+            
+        }
+    }
+}
